a8a311df2b61ec469e1d28ae465f457a6dea2a6a
joantriay88 <triayjoan@gmail.com>
2015-12-03 02:02:35 +0100

    I add the method join_video_data_no_redundant, this method create one file with all no redundant variables

--- WriteData.py
+++ WriteData.py
@@ -166,9 +166,102 @@ class WriteData:
 			accumulated_time_data = self.split_data(accumulated_time[i])
 			average_time_data = self.split_data(average_time[i])
 
-
-
 			line=name_video+","+duration_data+","+seek_for_data+","+seek_back_data+","+skipped_average_data+","+skipped_accumulated_data+","+replayed_average_data+","+replayed_accumulated_data+","+speedUp_data+","+speedDown_data+","+play_data+","+stop_data+","+pause_data+","+accumulated_time_data+","+average_time_data+"\n"
+			fileData.write(line)
+
+		fileData.close()
+		return
+
+	def join_video_data_no_redundant(self):
+		f=open("files/replayed_time_accumulated_video.txt", "r")
+		replayed_accumulated= f.readlines()
+		f.close()
+
+		f=open("files/replayed_time_average_video.txt", "r")
+		replayed_average= f.readlines()
+		f.close()
+
+		f=open("files/skipped_time_accumulated_video.txt", "r")
+		skipped_accumulated= f.readlines()
+		f.close()
+
+		f=open("files/skipped_time_average_video.txt", "r")
+		skipped_average= f.readlines()
+		f.close()
+
+		f=open("files/speedUp_average_video.txt", "r")
+		speedUp= f.readlines()
+		f.close()
+
+		f=open("files/speedDown_average_video.txt", "r")
+		speedDown= f.readlines()
+		f.close()
+
+		f=open("files/nr_times_seek_back_video.txt", "r")
+		seek_back= f.readlines()
+		f.close()
+
+		f=open("files/nr_times_seek_for_video.txt", "r")
+		seek_for= f.readlines()
+		f.close()
+
+		f=open("files/nr_times_plays_video.txt", "r")
+		play_times= f.readlines()
+		f.close()
+
+		f=open("files/nr_times_stops_video.txt", "r")
+		stop_times= f.readlines()
+		f.close()
+
+		f=open("files/nr_times_pauses_video.txt", "r")
+		pause_times= f.readlines()
+		f.close()
+
+		f=open("files/nr_times_speed_up_video.txt", "r")
+		speed_up_times= f.readlines()
+		f.close()
+
+		f=open("files/nr_times_speed_down_video.txt", "r")
+		speed_down_times= f.readlines()
+		f.close()
+		
+		f=open("files/accumulated_viewed_time_video.txt", "r")
+		accumulated_time= f.readlines()
+		f.close()
+
+		f=open("files/average_viewed_time_video.txt", "r")
+		average_time= f.readlines()
+		f.close()
+
+		f=open("files/duration_yt_video.txt", "r")
+		duration= f.readlines()
+		f.close()
+
+		
+
+		fileData = open("files/AllNoRedVariablesVideo.txt", "a")
+
+		fileData.write("Name,Duration,SkippedTimeAv,SkippedTimeAc,ReplayedTimeAv,ReplayedTimeAc,SpeedUpAv,SpeedDownAv,Plays,Stops,Pauses,SeekForTimes,SeekBackTimes,SpeedDownTimes,SpeedUpTimes,AccumulatedTime,AverageTime\n")
+
+		for i in range(len(seek_for)):
+			name_video, seek_for_data = self.split_first_data(seek_for[i])
+			duration_data=self.split_data(duration[i])
+			seek_back_data = self.split_data(seek_back[i])
+			skipped_average_data = self.split_data(skipped_average[i])
+			skipped_accumulated_data = self.split_data(skipped_accumulated[i])
+			replayed_average_data = self.split_data(replayed_average[i])
+			replayed_accumulated_data = self.split_data(replayed_accumulated[i])
+			speedUp_data = self.split_data(speedUp[i])
+			speedDown_data = self.split_data(speedDown[i])
+			play_data = self.split_data(play_times[i])
+			stop_data = self.split_data(stop_times[i])
+			pause_data = self.split_data(pause_times[i])
+			accumulated_time_data = self.split_data(accumulated_time[i])
+			average_time_data = self.split_data(average_time[i])
+			speedup_times_data= self.split_data(speed_up_times[i])
+			speeddown_times_data=self.split_data(speed_down_times[i])
+
+			line=name_video+","+duration_data+","+skipped_average_data+","+skipped_accumulated_data+","+replayed_average_data+","+replayed_accumulated_data+","+speedUp_data+","+speedDown_data+","+play_data+","+stop_data+","+pause_data+","+seek_for_data+","+seek_back_data+","+speeddown_times_data+","+speedup_times_data+","+accumulated_time_data+","+average_time_data+"\n"
 			fileData.write(line)
 
 		fileData.close()
